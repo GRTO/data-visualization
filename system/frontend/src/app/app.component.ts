402afeace8b8bfe6aa1dc5798e846a6aae1be7cb
Gerson Toribio <gerson.toribio@pucp.pe>
2019-10-27 00:53:44 -0500

    Fix library

--- system/frontend/src/app/app.component.ts
+++ system/frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import * as L from "leaflet";
+import jsonObject from "../assets/departements.json";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = 'frontend';
+  map: L.Map;
+  json;
+  options = {
+    layers: [
+      L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 18,
+        attribution: ""
+      })
+    ],
+    zoom: 7,
+    center: L.latLng(47.482019, -1)
+  };
+
+  constructor(private http: HttpClient) {}
+
+  onMapReady(map: L.Map) {
+    this.json = jsonObject;
+    L.geoJSON(this.json).addTo(map);
+  }
 }
